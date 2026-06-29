1be06b3340a9ca1953b137648ac6a38fce366de5
German Officer <youarenotgoingtogetmyemail@mail.com>
2026-06-29 10:55:51 +0300

    Decorators are created

---
+++ tests/CASES_AVAR.py
@@ -0,0 +1,28 @@
+CASES_FROM_CYRILLIC_TO_LATIN = [
+        ("Салам", "Salam"),
+        ("Школа", "Şkola"),
+        ("Зобазда роржунел, камилал цӀумал,Кисан нуж рачIарал мугIрузде бице?МугIрул тун рикIкIада, рагъда тIагIаралМагIарул васазул ракIал руго ниж.", "Zobazda rorƶunel, kamilal ꞩӀumal,Kisan nuƶ raçaral muⱨruzde biꞩe?Muⱨrul tun riⱪⱪada, raƣda ҭaⱨaralMaⱨarul vasazul raⱪal rugo niƶ."),
+        ("АаБбВвГгГъгъГьгьГIгI", "AaBbVvGgƢƣHhⱧⱨ"),
+        ("ДдЕеЖжЗзИиЙйКкКъкъКькьКIкI", "DdEeƵƶZzIiJjKkQqꝖꝗⱩⱪ"),
+        ("ЛлЛълъМмНнОоПпРрСсТтТIтI", "LlĻļMmNnOoPpRrSsTtҬҭ"),
+        ("УуФфХхХъхъХьхьХIхI", "UuFfXxӾӿҲҳЋћ"),
+        ("ЦцЦIцIЧчЧIчIШшЩщъЭэЮюЯя", "ꞨꞩⱫⱬCcÇçŞşŞŞşş'EeJujuJaja")
+]
+
+CASES_FROM_LATIN_TO_CYRILLIC = [
+        ("Salam", "Салам"),
+        ("Şkola", "Школа"),
+        ("Zobazda rorƶunel, kamilal ꞩӀumal,Kisan nuƶ raçaral muⱨruzde biꞩe?Muⱨrul tun riⱪⱪada, raƣda ҭaⱨaralMaⱨarul vasazul raⱪal rugo niƶ.", "Зобазда роржунел, камилал цӀумал,Кисан нуж рачIарал мугIрузде бице?МугIрул тун рикIкIада, рагъда тIагIаралМагIарул васазул ракIал руго ниж."),
+        ("AaBbVvGgƢƣHhⱧⱨ", "АаБбВвГгГъгъГьгьГIгI"),
+        ("DdEeƵƶZzIiJjKkQqꝖꝗⱩⱪ", "ДдЕеЖжЗзИиЙйКкКъкъКькьКIкI"),
+        ("LlĻļMmNnOoPpRrSsTtҬҭ", "ЛлЛълъМмНнОоПпРрСсТтТIтI"),
+        ("UuFfXxӾӿҲҳЋћ", "УуФфХхХъхъХьхьХIхI"),
+        ("ꞨꞩⱫⱬCcÇçŞşŞŞşş'JujuJaja", "ЦцЦIцIЧчЧIчIШшЩщъЮюЯя")
+
+]
+
+CASES_LATIN = [
+    "Salam",
+    "Şkola",
+    "Zobazda rorƶunel, kamilal ꞩӀumal,Kisan nuƶ raçaral muⱨruzde biꞩe?Muⱨrul tun riⱪⱪada, raƣda ҭaⱨaralMaⱨarul vasazul raⱪal rugo niƶ."
+]
--- tests/test_avar.py
+++ tests/test_avar.py
@@ -1,19 +1,31 @@
 from tranliterator import *
+from tests.CASES_AVAR import *
 
-def test_simple_word_from_cyrillic_to_latin():
-    text = "Салам"
-    assert cyrillic_to_latin(text) == "Salam"
+import pytest
 
-def test_simple_word_from_latin_to_cyrillic():
-    text = "Salam"
-    assert latin_to_cyrillic(text) == "Салам"
+@pytest.mark.parametrize(
+        "text, expected",
+        CASES_FROM_CYRILLIC_TO_LATIN
+)
+def test_simple_word_from_cyrillic_to_latin(text, expected):
+    assert cyrillic_to_latin(text) == expected
 
-def test_retransliteration():
-    text = "Salam"
-    text = latin_to_cyrillic(text)
-    text = cyrillic_to_latin(text)
+@pytest.mark.parametrize(
+        "text, expected",
+        CASES_FROM_LATIN_TO_CYRILLIC
+)
+def test_simple_word_from_latin_to_cyrillic(text, expected):
+    assert latin_to_cyrillic(text) == expected
 
-    assert text == "Salam"
+@pytest.mark.parametrize(
+        "text",
+        CASES_LATIN
+)
+def test_retransliteration(text):
+    latin_text = latin_to_cyrillic(text)
+    final_text = cyrillic_to_latin(latin_text)
+
+    assert final_text == text
 
 
     
